27aceb68d64f311358de767a01a0398dc6ade6c6
GimYoungPhil <youngphil.gim@gmail.com>
2021-06-21 16:52:05 +0900

    fixed

--- projects/api/src/index.js
+++ projects/api/src/index.js
@@ -1,21 +1,52 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
 
-const app = express();
 const port = process.env.PORT || 4000;
 
+const notes = [
+  { id: '1', content: 'This is a note', author: 'Adam Scott' },
+  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
+  { id: '3', content: 'Ho hey look', author: 'Riley Harrison' },
+];
+
 const typeDefs = gql`
+  type Note {
+    id: ID!
+    content: String!
+    author: String!
+  }
+
   type Query {
     hello: String,
+    notes: [Note!]!
+    note(id: ID!): Note!
+  }
+
+  type Mutation {
+    newNote(content: String!): Note!
   }
 `;
 
 const resolvers = {
   Query: {
     hello: () => 'Hello world',
+    notes: () => notes,
+    note: (parent, args) => notes.find(note => note.id === args.id),
   },
+  Mutation: {
+    newNote: (parent, args) => {
+      const noteValue = {
+        id: String(notes.length + 1),
+        content: args.content,
+        author: 'anonymous',
+      };
+      notes.push(noteValue);
+      return noteValue;
+    },
+  }
 };
 
+const app = express();
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path: '/api' });
 
