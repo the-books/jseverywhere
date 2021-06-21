8263282995c1071b770d35e7cc8a4286327a05b7
GimYoungPhil <youngphil.gim@gmail.com>
2021-06-21 14:58:06 +0900

    add ch04

--- projects/api/src/index.js
+++ projects/api/src/index.js
@@ -1,9 +1,26 @@
 const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
 const app = express();
 const port = process.env.PORT || 4000;
+
+const typeDefs = gql`
+  type Query {
+    hello: String,
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: '/api' });
+
 app.get('/', (req, res) => res.send('Hello World!!!'));
 
-app.listen(port, () => 
-  console.log(`Server running at http://localhost:${port}`)
+app.listen({ port }, () => 
+  console.log(`GraphQL Server running at http://localhost:${port}/${server.graphqlPath}`)
 );
