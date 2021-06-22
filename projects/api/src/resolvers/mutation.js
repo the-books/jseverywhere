c5fb1aed8e75dc516aca1483454ad2499d605618
GimYoungPhil <youngphil.gim@gmail.com>
2021-06-22 16:35:44 +0900

    fixed

--- projects/api/src/resolvers/mutation.js
+++ projects/api/src/resolvers/mutation.js
@@ -1,3 +1,5 @@
+const models = require("../models");
+
 module.exports = {
   newNote: async (parent, args, { models }) => {
     return await models.Note.create({
@@ -5,4 +7,14 @@ module.exports = {
       author: 'Adam Scott',
     });
   },
+  updateNote: async (parent, args, { models }) => {
+    return await models.Note.updateOne({
+      id: args.id
+    },{
+      content: args.content,
+    });
+  },
+  deleteNote: async (parent, args, { models }) => {
+    return await models.Note.de
+  }
 };
