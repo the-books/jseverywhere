36d5ca23949eb05eed850e90f9ef7fb63a734548
GimYoungPhil <youngphil.gim@gmail.com>
2021-06-22 10:35:16 +0900

    fixed

--- projects/api/src/index.js
+++ projects/api/src/index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const { ApolloServer, gql } = require('apollo-server-express');
+require('dotenv').config();
+
+const db = require('./db');
+const models = require('./models');
 
 const port = process.env.PORT || 4000;
-
-const notes = [
-  { id: '1', content: 'This is a note', author: 'Adam Scott' },
-  { id: '2', content: 'This is another note', author: 'Harlow Everly' },
-  { id: '3', content: 'Ho hey look', author: 'Riley Harrison' },
-];
+const DB_HOST = process.env.DB_HOST;
 
 const typeDefs = gql`
   type Note {
@@ -30,28 +29,31 @@ const typeDefs = gql`
 const resolvers = {
   Query: {
     hello: () => 'Hello world',
-    notes: () => notes,
-    note: (parent, args) => notes.find(note => note.id === args.id),
+    notes: async () => {
+      return await models.Note.find();
+    },
+    note: async (parent, args) => {
+      return await models.Note.findById(args.id);
+    },
   },
   Mutation: {
-    newNote: (parent, args) => {
-      const noteValue = {
-        id: String(notes.length + 1),
+    newNote: async (parent, args) => {
+      return await models.Note.create({
         content: args.content,
-        author: 'anonymous',
-      };
-      notes.push(noteValue);
-      return noteValue;
+        author: 'Adam Scott',
+      });
     },
   }
 };
 
 const app = express();
+db.connect(DB_HOST);
+
 const server = new ApolloServer({ typeDefs, resolvers });
 server.applyMiddleware({ app, path: '/api' });
 
 app.get('/', (req, res) => res.send('Hello World!!!'));
 
 app.listen({ port }, () => 
-  console.log(`GraphQL Server running at http://localhost:${port}/${server.graphqlPath}`)
+  console.log(`GraphQL Server running at http://localhost:${port}${server.graphqlPath}`)
 );
