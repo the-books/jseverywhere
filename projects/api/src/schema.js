5f1686f095e41ee7488852f355849bfdb01658d6
GimYoungPhil <youngphil.gim@gmail.com>
2021-06-23 14:36:47 +0900

    scalar DateTime

--- projects/api/src/schema.js
+++ projects/api/src/schema.js
@@ -1,11 +1,23 @@
 const { gql } = require('apollo-server-express');
 
 module.exports = gql`
+  scalar DateTime
+
   type Note {
     id: ID!
     content: String!
     author: String!
+    createdAt: DateTime!
+    updatedAt: DateTime!
   }
+
+  # type User {
+  #   id: ID!
+  #   username: String!
+  #   email: String!
+  #   avatar: String
+  #   notes: [Note!]!
+  # }
 
   type Query {
     hello: String,
@@ -15,7 +27,7 @@ module.exports = gql`
 
   type Mutation {
     newNote(content: String!): Note!
-    udpateNote(id: ID!, content: String!): Note!
+    updateNote(id: ID!, content: String!): Note!
     deleteNote(id: ID!): Boolean!
   }
 `;
